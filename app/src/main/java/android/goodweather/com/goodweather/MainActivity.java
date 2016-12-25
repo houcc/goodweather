29fda5beaeabe54ccea38191667620870f926349
houcc <1358453452@qq.com>
2016-12-25 14:12:38 +0800

    first commit

---
+++ app/src/main/java/android/goodweather/com/goodweather/MainActivity.java
@@ -0,0 +1,13 @@
+package android.goodweather.com.goodweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
